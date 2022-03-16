a7d110d673ea5d37d02d53df836b0435f5103901
Bruno Peruchi <peruchib@gmail.com>
2022-03-15 23:30:02 -0300

    refatoração do código por completo; input unificado para simplificar o uso

--- script.js
+++ script.js
@@ -1,54 +1,25 @@
 let financas = []
 let qntFinancas = financas.length
-let entradas = []
-let qntEntradas = entradas.length
-let saidas = []
-let qntSaidas = saidas.length
 let valorTotal = 0
 
-function addEntrada() {
-    let tipo = '+'
-    let inputEntrada = document.getElementById('inputEntrada')
-    let entrada = Number(inputEntrada.value)
-    let mostraEntradas = document.getElementById('mostraEntradas')
+function addValor() {
+    let inputValor = document.getElementById('inputValor')
+    let valorNum = Number(inputValor.value)
 
-    inputEntrada.value = ''
-    inputEntrada.focus()
+    console.log(valorNum)
 
-    if (entrada < 0 || !entrada) {
-        window.alert('Entrada inválida!')
+    if (!valorNum || valorNum == 0) {
+        window.alert('Valor inválido!')
+    } else if (valorNum > 0) {
+        atualizaTotal(valorNum, "+")
     } else {
-        let option = document.createElement('option')
-        option.value = `${qntEntradas}`
-        option.text = `R$ ${entrada.toFixed(2).replace('.', ',')}`
-        mostraEntradas.insertBefore(option, mostraEntradas.firstChild)
-        entradas.push(entrada)
-    
-        atualizaTotal(entrada, tipo)
+        atualizaTotal(valorNum, "-")
     }
+
+    inputValor.value = ''
+    inputValor.focus()
 }
 
-function addSaida() {
-    let tipo = '-'
-    let inputSaida = document.getElementById('inputSaida')
-    let saida = Number(inputSaida.value)
-    let mostraSaidas = document.getElementById('mostraSaidas')
-
-    inputSaida.value = ''
-    inputSaida.focus()
-
-    if (saida < 0 || !saida) {
-        window.alert('Saída inválida!')
-    } else {
-        let option = document.createElement('option')
-        option.value = `${qntSaidas}`
-        option.text = `R$ ${saida.toFixed(2).replace('.', ',')}`
-        mostraSaidas.insertBefore(option, mostraSaidas.firstChild)
-        saidas.push(saida)
-    
-        atualizaTotal(saida, tipo)
-    }
-}
 function atualizaTotal(valor, tipo) {
     let boxTotal = document.getElementById('boxTotal')
     let total = document.querySelector('#boxTotal span')
@@ -57,27 +28,26 @@ function atualizaTotal(valor, tipo) {
     let option = document.createElement('option')
     option.value = `${qntFinancas}`
     option.text = `R$ ${valor.toFixed(2).replace('.', ',')}`
-    
+
     if (tipo == '+') {
-        valorTotal += valor
         option.style.color = '#26bf00'
     } else if (tipo == '-') {
-        valorTotal -= valor
         option.style.color = '#e90000'
     }
 
+    valorTotal += valor
     mostraTotal.insertBefore(option, mostraTotal.firstChild)
     financas.push(valor)
-    
-    if (valorTotal > 0) {
+
+    if (valor > 0) {
         total.style.color = '#26bf00'
-        boxTotal.style.boxShadow = '-3px -3px #26bf00'
-    } else if (valorTotal < 0) {
+        boxTotal.style.boxShadow = '-2px -2px #26bf00'
+    } else if (valor < 0) {
         total.style.color = '#e90000'
         boxTotal.style.boxShadow = '2px 2px #e90000'
-    } else if (valorTotal == 0) {
+    } else if (valor == 0) {
         total.style.color = '#fff'
         boxTotal.style.boxShadow = ''
     }
-    total.innerHTML = `R$ ${valorTotal.toFixed(2).replace('.', ',').replace('-', '')}`
+    total.innerHTML = `R$ ${valor.toFixed(2).replace('.', ',').replace('-', '')}`
 }
