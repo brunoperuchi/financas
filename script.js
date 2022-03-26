681d3a9ad830582ebbea5cf35614f2079506085d
Bruno Peruchi <peruchib@gmail.com>
2022-03-26 00:49:39 -0300

    js mais legível e reduzido

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ let finances = []
 let tags = []
 
 function addTag() {
-    let inputTag = document.getElementById('tag-input')
-    let tagName = inputTag.value.toUpperCase()
+    const inputTag = document.getElementById('tag-input')
+
+    const tagName = inputTag.value.toUpperCase()
     
     if (tags.includes(tagName)) {
         window.alert('Marcador já cadastrado!')
@@ -21,12 +22,13 @@ function addTag() {
 }
 
 function refreshTagSelect() {
-    let tagSelect = document.getElementById('tag-select')
+    const tagSelect = document.getElementById('tag-select')
+
     tagSelect.innerHTML = ''
     
     tags.sort((a, b) => a.localeCompare(b))
     
-    tags.forEach(function(tag, index) {
+    tags.forEach((tag, index) => {
         let option = document.createElement('option')
         option.value = `${index}`
         option.text = `${tag}`
@@ -37,18 +39,19 @@ function refreshTagSelect() {
 }
 
 function addFinance() {
-    let inputDate = document.getElementById('date-input')
-    let date = inputDate.value
-    let inputValue = document.getElementById('value-input')
-    let valueNum = Number(inputValue.value)
-    let tagSelect = document.getElementById('tag-select')
-    let tag = tagSelect.options[tagSelect.selectedIndex].text
+    const inputDate = document.getElementById('date-input')
+    const inputValue = document.getElementById('value-input')
+    const tagSelect = document.getElementById('tag-select')
+
     
+    const valueNum = Number(inputValue.value)
+    const tag = tagSelect.options[tagSelect.selectedIndex].text
+
     if (!valueNum || valueNum == 0) {
         window.alert('Valor inválido!')
     } else {
         finances.push({
-            date: date,
+            date: inputDate.value,
             value: valueNum,
             tag: tag
         })
@@ -61,23 +64,21 @@ function addFinance() {
 }
 
 function refreshShowAll() {
-    let showTotal = document.getElementById('show-total')
+    const showTotal = document.getElementById('show-total')
     showTotal.innerHTML = ''
 
     let valueTotal = 0
     
     finances.sort(descendingOrderDate)
 
-    finances.forEach(function(finance, index) {
-        let date = finance.date
-        let value = finance.value
-        let tag = finance.tag
+    finances.forEach((finance, index) => {
+        const value = finance.value
 
         valueTotal += value
 
-        let option = document.createElement('option')
+        const option = document.createElement('option')
         option.value = `${index}`
-        option.text = `${convertDate(date)} - R$ ${convertCoin(value)} - ${tag}`
+        option.text = `${convertDate(finance.date)} - R$ ${convertCoin(value)} - ${finance.tag}`
         option.style.color = value > 0 ? colorGreen : colorRed
         
         showTotal.insertBefore(option, showTotal.firstChild)
@@ -87,20 +88,23 @@ function refreshShowAll() {
 }
 
 function refreshBoxTotal(value) {
-    let boxTotal = document.getElementById('box-total')
-    let total = document.querySelector('#box-total span')
-
+    const total = document.querySelector('#box-total span')
+    
     total.innerHTML = `R$ ${convertCoin(value)}`
     
+    const totalStyle = total.style
+    const boxTotalStyle = document.getElementById('box-total').style
+    
+
     if (value > 0) {
-        total.style.color = colorGreen
-        boxTotal.style.boxShadow = `-2px -2px ${colorGreen}`
+        totalStyle.color = colorGreen
+        boxTotalStyle.boxShadow = `-2px -2px ${colorGreen}`
     } else if (value < 0) {
-        total.style.color = colorRed
-        boxTotal.style.boxShadow = `2px 2px ${colorRed}`
+        totalStyle.color = colorRed
+        boxTotalStyle.boxShadow = `2px 2px ${colorRed}`
     } else if (value == 0) {
-        total.style.color = '#fff'
-        boxTotal.style.boxShadow = ''
+        totalStyle.color = '#fff'
+        boxTotalStyle.boxShadow = ''
     }
 }
 
